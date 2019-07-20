2f6f3f698c937d7abde52f3ad605fa3bfbff754c
Eric Mulek <eric.mulek@surescripts.com>
2019-07-20 11:37:16 -0500

    Initial commit

---
+++ src/main/groovy/com/ericmulek/springthymleaf/SpringthymleafApplication.groovy
@@ -0,0 +1,13 @@
+package com.ericmulek.springthymleaf
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringthymleafApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringthymleafApplication, args)
+    }
+
+}
---
+++ src/test/groovy/com/ericmulek/springthymleaf/SpringthymleafApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.ericmulek.springthymleaf
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class SpringthymleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
